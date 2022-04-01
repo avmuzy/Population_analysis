38042ad79b52faff2859cf444b45fc0901264dc8
avms <andrevmuzy@gmail.com>
2022-03-31 21:53:01 -0300

    adding parameters

--- BMI.py
+++ BMI.py
@@ -3,4 +3,14 @@
 height = float(input('Enter your height in meters: '))
 weight = float(input('Enter your weight in kilograms: '))
 bmi = weight / height**2
-print(bmi)
+print('Tour BMI is: %4f' % bmi)
+
+if bmi < 16:
+    print('Severe Thinness')
+elif bmi < 17:
+    print('Moderate Thinness')
+elif bmi < 18.5:
+    print('Mild Thinness')
+elif bmi < 25:
+    print('Normal')
+
