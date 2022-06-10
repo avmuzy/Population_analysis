5f62159203911c252066060a78551f9c987bf5b8
avms <andrevmuzy@gmail.com>
2022-06-09 22:53:15 -0300

    bmi

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 x = pd.read_csv('/home/avms/data_base.csv')
 df = pd.DataFrame(x)
+
 print(x)
 print(df[['Kilograms', 'Centimeters']])
 print(type(x))
-bmi = df['BMI'] = df['Kilograms'] / df['Centimeters']
+bmi = df['BMI'] = df['Kilograms'] / ((df['Centimeters']/100)**2)
 print(bmi)
