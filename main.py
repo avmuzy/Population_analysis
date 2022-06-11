dea49870fe0f38cff88587d4dc426df21aa88bd3
avms <andrevmuzy@gmail.com>
2022-06-10 21:51:14 -0300

    main

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 x = pd.read_csv('/home/avms/data_base.csv')
 df = pd.DataFrame(x)
-
+print(x)
+print(df[['Kilograms', 'Centimeters']])
+print(type(x))
+bmix = df['BMI'] = df['Kilograms'] / ((df['Centimeters']/100)**2)
+print(bmix)
