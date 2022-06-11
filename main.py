b418cdb01efc7dea3f8156ca4466616b944f7405
avms <andrevmuzy@gmail.com>
2022-06-11 18:55:27 -0300

    main

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ print(df[['Kilograms', 'Centimeters']])
 print(type(x))
 bmix = df['BMI'] = df['Kilograms'] / ((df['Centimeters']/100)**2)
 print(bmix)
+idade = df['Age']
+fig, ax = plt.subplots()
+ax.pie(idade, labels=bmix, autopct='%1.1f%%', shadow=True, startangle=90)
+ax.axis('equal')
+plt.show()
