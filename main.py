bad0350f9a0ec946a52b92453a03eb324e952c77
avms <andrevmuzy@gmail.com>
2022-06-12 16:14:22 -0300

    main

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ fig, ax = plt.subplots()
 ax.pie(idade, labels=bmix, autopct='%1.1f%%', shadow=True, startangle=90)
 ax.axis('equal')
 plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(idade, bmix)
+ax.set(xlabel='Age', ylabel='BMI',
+       title='AGE X BMI')
+ax.grid()
+
+fig.savefig("test.png")
+plt.show()
