2cad97cea7005427be4fa5b133db7a1742150d94
avms <andrevmuzy@gmail.com>
2022-06-14 17:00:01 -0300

    plots

--- plots.py
+++ plots.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 x = pd.read_csv('new_data_base.csv')
 df = pd.DataFrame(x)
 print(x)
+age = df["Age"]
+kilo = df['Kilograms']
 
 fig, ax = plt.subplots()
-ax.plot(['Age'], ['Kilograms'])
+ax.plot(age, kilo)
 
 ax.set(xlabel='Age', ylabel='Kilograms',
        title='Age X kilograms')
