76d4713b88bdabaaa0b56c3b99998ed1ce194e01
avms <andrevmuzy@gmail.com>
2022-06-17 18:32:06 -0300

    correcting

--- BMI.py
+++ BMI.py
@@ -1,4 +1,11 @@
 # simple BMI calculation
+import pandas as pd
+import matplotlib.pyplot as plt
+x = pd.read_csv('new_data_base.csv')
+df = pd.DataFrame(x)
+age = df["Age"]
+kilo = df['Kilograms']
+bmix = df['BMI'] = df['Kilograms'] / ((df['Centimeters']/100)**2)
 
 height = float(input('Enter your height in meters: '))
 weight = float(input('Enter your weight in kilograms: '))
