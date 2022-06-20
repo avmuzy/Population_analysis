bbc23880a939b6d9da8e999f5e51c1ba4ae2424f
avms <andrevmuzy@gmail.com>
2022-06-20 17:46:47 -0300

    simplify chained comparison

--- BMI.py
+++ BMI.py
@@ -13,19 +13,19 @@ weight = float(input('Enter your weight in kilograms: '))
 bmi = weight / height**2
 print('Your BMI is: %4f' % bmi)
 
-if 16 > bmi > 17:
+if 16 > bmi > 16.9:
     print('Severe Thinness')
 elif 17 > bmi > 18.5:
     print('Moderate Thinness')
-elif 18.5 > bmi > 25:
+elif 18.5 > bmi > 24.9:
     print('Mild Thinness')
-elif 25 > bmi > 30:
+elif 25 > bmi > 29.9:
     print('Normal')
-elif 30 > bmi > 35:
+elif 30 > bmi > 34.9:
     print('Overweight')
 elif 35 > bmi > 40:
     print('Obese Class I')
-elif bmi < 40:
+elif 40 > bmi < 49.9:
     print('Obese Class II')
 else:
     print('Obese Class III')
