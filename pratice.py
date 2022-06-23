c9f711dc4bd1637c51f9d533308875b4a3fe553f
avms <andrevmuzy@gmail.com>
2022-06-23 19:25:23 -0300

    importing data

--- pratice.py
+++ pratice.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import numpy as np
 import csv
 import matplotlib.pyplot as plt
+
 x = pd.read_csv('new_data_base.csv')
 df = pd.DataFrame(x)
 age = df["Age"]
 kilo = df['Kilograms']
-bmix = df['BMI'] = df['Kilograms'] / ((df['Centimeters']/100)**2)
+bmix = df['BMI'] = df['Kilograms'] / ((df['Centimeters'] / 100) ** 2)
 print(x.head())
 maxBmi = df['BMI'].max()
 minBmi = df['BMI'].min()
 print(maxBmi, minBmi)
+
+
