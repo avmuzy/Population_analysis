b57de5c225ce2cc4f9e0fd380821b8c75b51e7d7
avms <andrevmuzy@gmail.com>
2022-06-27 17:22:10 -0300

    reading csv

--- analysis.py
+++ analysis.py
@@ -1,3 +1,10 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+
+x = pd.read_csv('/home/avms/PycharmProjects/Population_analysis/new_data_base.csv')
+df = pd.DataFrame(x)
+age = df["Age"]
+kilo = df['Kilograms']
+bmi = df['BMI'] = df['Kilograms'] / ((df['Centimeters'] / 100) ** 2)
+print(bmi)
