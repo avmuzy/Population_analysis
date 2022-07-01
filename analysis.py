c53ef71f89e9e5c19e8832b84ac04ec94b604d68
avms <andrevmuzy@gmail.com>
2022-07-01 20:06:30 -0300

    new variables

--- analysis.py
+++ analysis.py
@@ -43,4 +43,8 @@ severeThinness = (list(filter(lambda b: b < 16.9, bmi)))
 moderateThinness = (list(filter(lambda b: 17 < b < 18.4, bmi)))
 mildThinness = (list(filter(lambda b: 18.5 < b < 24.9, bmi)))
 normal = (list(filter(lambda b: 25 < b < 29.9, bmi)))
+overweight = (list(filter(lambda b: 30 < b < 34.9, bmi)))
+obeseClassI = (list(filter(lambda b: 35 < b < 39.9, bmi)))
+obeseClassII = (list(filter(lambda b: 40 < b < 49.9, bmi)))
+obeseClassIII = (list(filter(lambda b: b > 50, bmi)))
 
