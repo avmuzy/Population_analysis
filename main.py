e552666389f07049fc73ea4abc0e3e020549486e
avms <andrevmuzy@gmail.com>
2022-07-02 19:28:39 -0300

    # data for a pie plot

--- main.py
+++ main.py
@@ -47,3 +47,7 @@ print('Obese Class II:  ', len(obeseClassII), obeseClassII)
 print('Obese Class III: ', len(obeseClassIII), obeseClassIII)
 print(len(severeThinness), len(moderateThinness), len(mildThinness), len(normal), len(overweight),
       len(obeseClassI), len(obeseClassII), len(obeseClassIII))
+
+# data for a pie plot
+bmiNames = ['Severe Thinness', 'Moderate Thinness', 'Mild Thinness', 'Normal', 'Obese Class I',
+            'Obese Clas II', 'Obese Class III']
