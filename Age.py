b8f0b31c266af7ca4ef82aaecff767981398708d
avms <andrevmuzy@gmail.com>
2022-07-03 11:43:16 -0300

    defining the column to be analyzed

--- Age.py
+++ Age.py
@@ -2,3 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statistics
 from functools import reduce
+
+# reading csv
+x = pd.read_csv('/home/avms/data_base.csv')
+df = pd.DataFrame(x)
+# defining the column to be analyzed
+age = df["Age"]
