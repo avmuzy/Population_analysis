097fc81de0945ee364072902325896b046f35015
avms <andrevmuzy@gmail.com>
2022-07-03 11:46:56 -0300

    Age group

--- Age.py
+++ Age.py
@@ -8,3 +8,13 @@ x = pd.read_csv('/home/avms/data_base.csv')
 df = pd.DataFrame(x)
 # defining the column to be analyzed
 age = df["Age"]
+
+# Age group
+children = (list(filter(lambda a: a <= 14, age)))
+print(len(children), children)
+youth = list(filter(lambda a: 15 <= a <= 24, age))
+print(len(youth), youth)
+adults = list(filter(lambda a: 25 <= a <= 64, age))
+print(len(adults), adults)
+senior = list(filter(lambda a: a >= 65, age))
+print(len(senior), senior)
