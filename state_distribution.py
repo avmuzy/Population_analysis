448d066f6b61f88beb79b126186d63ab87be679e
avms <andrevmuzy@gmail.com>
2022-07-05 19:07:49 -0300

    reading csv

--- state_distribution.py
+++ state_distribution.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statistics
 from functools import reduce
+
+x = pd.read_csv('/home/avms/PycharmProjects/Population_analysis/new_data_base.csv')
+df = pd.DataFrame(x)
+print(x)
